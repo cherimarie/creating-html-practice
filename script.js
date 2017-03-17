980aa35d26550e4b899f046f8c2bf82181d0fe19
Cheri Allen <cherimarie@gmail.com>
2017-03-16 17:37:42 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("let's get to work! ");
+
+var products = [
+  {name: "etro", price: 19.99, image: "img.jpg"},
+  {name: "plaid", price: 16.99, image: "img.jpg"},
+  {name: "wool", price: 22.99, image: "img.jpg"}
+]
